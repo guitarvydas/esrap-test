1f4332c1108a50441ed66c375f2660a5d3b6d3ec
Paul Tarvydas <guitarvydas@gmail.com>
2019-07-16 11:39:44 -0400

    changed then unchanged transcoder

--- transcoder.lisp
+++ transcoder.lisp
@@ -1,10 +1,3 @@
-;; 1. load peg.lisp - this is the LISP source code for a PEG parser (based on LISP esrap) 
-;; 2. run the parser on sw-toos-in-pascal.peg which generates a LISP source file for a Pascal parser
-;;    (not all of pascal, just enough to transcode the sw tools source code (src.p))
-;; 3. run the new parser (subset of pascal) on src.p to transcode src.p into some other language
-
-;; This program (@main) performs step 3. (After the code in step 2 has been loaded)
-
 (defun @parse-swtools (&key (swtools-source nil) (result-filename nil))
   (unless (and swtools-source result-filename)
     (error "no sw tools Pascal filename specified"))
